e336e3a42ec0a81775ee42171caf6604f9c09e22
gomaa <gogochemist35@gmail.com>
2023-09-13 23:27:56 +0300

    change code

--- problem16 all words from AAA to ZZZ/problem16 all words from AAA to ZZZ.cpp
+++ problem16 all words from AAA to ZZZ/problem16 all words from AAA to ZZZ.cpp
@@ -4,26 +4,34 @@ using namespace std;
 
 
 
+
+char getLetter(int num)
+{
+	return char(num + 64);
+}
+
 void printInvertedPattern()
 {
-    for (int x = 65; x <=90; x++)
-    {
-        cout << "=====\n";
-        cout << char(x) << endl;
-        cout << "=====\n";
-        
-        for (int i = 65; i <=90; i++)
-        {
-            cout << "=====\n";
-            cout << char(i) << endl;
-            cout << "=====\n";
-            
-            for (int y = 65; y <= 90; y++)
-            {
-                cout<<char(x)<<char(i)<<char(y)<<"\n";
-            }
-        }
-    }
+	for (int i = 1; i <= 26; i++)
+	{
+		cout << "==============\n";
+		cout << getLetter(i);
+		cout << "==============\n";
+
+		for (int x = 1; x <= 26; x++)
+		{
+			cout << "==============\n";
+			cout << getLetter(x);
+			cout << "==============\n";
+
+			for (int y = 1; y <= 26; y++)
+			{
+				cout << getLetter(i)<< getLetter(x)<< getLetter(y)<<endl;
+			}
+			
+		}
+		
+	}
 }
 
 int main()
@@ -32,3 +40,24 @@ int main()
 
     return 0;
 }
+
+
+/*
+void printLetterPattern(int num)
+{
+	for (int i = 1; i <= num; i++)
+	{
+		for (int x = 1; x <= i; x++)
+		{
+			cout << getLetter(i);
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	printLetterPattern(readNumber("Enter number?"));
+
+	return 0;
+}*/
